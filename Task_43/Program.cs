1a63538e01b0f2eda6fc987fe40dbd8c44cbda41
Anna <vitani2007@mail.ru>
2023-07-21 17:52:03 +0300

    Task 43 solved

---
+++ Task_43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+void FindPointOfIntersection(double k1, double b1, double k2, double b2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k2 * x + b2;
+    Console.WriteLine($"({x}; {y})");
+}
+
+double GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double k1 = GetInput("Задайте углой коэффициент первой прямой: ");
+double b1 = GetInput("Задайте точку на оси ординат, через которую будет проходить первая прямая: ");
+double k2 = GetInput("Задайте углой коэффициент второй прямой: ");
+double b2 = GetInput("Задайте точку на оси ординат, через которую будет проходить вторая прямая: ");
+
+Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ");
+FindPointOfIntersection(k1, b1, k2, b2);
